c9f1bdaf5ea4a43261f7f91ec53aea236d8d8272
Stanislav Dobrovolschii <stasdobrovolskiy@gmail.com>
2018-09-20 01:21:22 +0200

    add schema to entities

--- lib/releases/entities/release.rb
+++ lib/releases/entities/release.rb
@@ -1,2 +1,12 @@
 class Release < Hanami::Entity
+  attributes do
+    attribute :id,            Types::Int
+    attribute :version,       Types::String
+    attribute :title,         Types::String
+    attribute :summary_raw,   Types::String
+    attribute :summary_html,  Types::String
+    attribute :released_at,   Types::Time
+    attribute :created_at,    Types::Time
+    attribute :updated_at,    Types::Time
+  end
 end
