43bea9f8579899531480dd58e58130a83cbffaa2
Stanislav Dobrovolschii <stasdobrovolskiy@gmail.com>
2018-09-20 01:21:22 +0200

    add schema to entities

--- lib/releases/entities/release_tag.rb
+++ lib/releases/entities/release_tag.rb
@@ -1,2 +1,7 @@
 class ReleaseTag < Hanami::Entity
+  attributes do
+    attribute :id,          Types::Strict::Int
+    attribute :release_id,  Types::Strict::Int
+    attribute :tag_id,      Types::Strict::Int
+  end
 end
--- lib/releases/entities/tag.rb
+++ lib/releases/entities/tag.rb
@@ -1,2 +1,8 @@
 class Tag < Hanami::Entity
+  attributes do
+    attribute :id,    Types::Int
+    attribute :name,  Types::Strict::String
+    attribute :slug,  Types::Strict::String
+    attribute :color, Types::Strict::String
+  end
 end
